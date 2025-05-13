90dd03283b0f9aa30cbf128c59a515dc1cf9774c
Bracha Heller <b7164553@gmail.com>
2025-05-13 21:37:33 +0300

    Mechonat Mashkahot

---
+++ src/App.jsx
@@ -0,0 +1,38 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+ 
+
+
+
+
+
+
+  return (
+    <div>
+      <div className='massage'>ברוכים הבאים</div>
+      <div className='right machine'>
+        {/* קומפוננטה ראשונה - שלום למשתמש*/}
+        <div className='hello'> </div>
+           {/* קומפוננטה שניה - מטבעות שהלקוח מכניס*/}
+           <div className=''></div>
+
+
+      </div>
+
+
+
+
+
+
+
+
+
+    </div>
+
+  );
+}
+
+export default App;
